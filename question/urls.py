3410335901a2fc9426b1262b3e2a6e23dc4e0852
Jimin Kim <0305jimin@gmail.com>
2021-08-03 21:51:20 +0900

    TEST-3 Feat: question_list Fix:question_detail

--- question/urls.py
+++ question/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('create/', views.question_create, name='question_create'),
     path('<int:pk>/edit/', views.question_edit, name='question_edit'),
     path('<int:pk>/delete/', views.question_delete, name='question_delete'),
-    
-    path('create/', views.comment_create, name='comment_create'),
-    path('<int:pk>/edit/', views.comment_edit, name='comment_edit'),
-    path('<int:pk>/delete/', views.comment_delete, name='comment_delete'),
+
+    path('comment/create/', views.comment_create, name='comment_create'),
+    path('comment/<int:pk>/edit/', views.comment_edit, name='comment_edit'),
+    path('comment/<int:pk>/delete/', views.comment_delete, name='comment_delete'),
 ]
