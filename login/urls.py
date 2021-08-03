6a8fc4a00a5584a1e97a81fffa181554c1773552
Minji Kwak <beinte0419@snu.ac.kr>
2021-08-03 22:21:16 +0900

    TEST-4 Feat 1차 로그인/회원가입 기능 구현

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 app_name = 'login'
 
 urlpatterns = [
-
+    path('', views.user_main, name='user_main'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='login/main.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.conf import settings
+# from django.contrib.auth import login as auth_login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from . import models, forms
+from django.contrib import auth
 
-# Create your views here.
+
+def user_main(request):
+    ctx = {"user_main": user_main}
+    return render(request, 'login/main.html', context=ctx)
+
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = models.User.objects.create_user(
+                username=request.POST['username'], password=request.POST['password1'], nickname=request.POST['nickname'], email=request.POST['email'])
+            auth.login(request, user)
+            return redirect('login:user_main')
+        return render(request, 'login/signup.html')
+
+    return render(request, 'login/signup.html')
+
+# class AccountCreateView(CreateView):
+#     model = models.User
+#     form_class = forms.UserCustomForm
+#     success_url = reverse_lazy('login:user_main')
+#     template_name = 'login/signup.html'
+
+
+@login_required
+def profile(request):
+    return(request, 'login/profile.html')
