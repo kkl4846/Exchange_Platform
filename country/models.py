2fa14bbec09bc10bf397d18cb1db116694630b56
Minji Kwak <beinte0419@snu.ac.kr>
2021-08-03 16:09:19 +0900

    TEST-1 Country 모델 추가

--- country/models.py
+++ country/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=50, verbose_name='나라 이름')
+    visa = models.TextField(blank=True, verbose_name='비자 발급 방법')
+    lifestyle = models.TextField(blank=True, verbose_name='현지 생활 및 치안')
+    money = models.TextField(blank=True, verbose_name='물가 및 체류 비용')
+    culture = models.TextField(blank=True, verbose_name='문화적 특징')
+    covid_info = models.TextField(blank=True, verbose_name='코로나 정보')
--- login/models.py
+++ login/models.py
@@ -6,7 +6,7 @@ app_name = 'user'
 
 
 class User(AbstractBaseUser):
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, verbose_name='닉네임')
     university = models.ForeignKey(
-        'domestic.Domestic', on_delete=models.SET_NULL, null=True, blank=True)
-    user_email = models.EmailField(blank=True)
+        'domestic.Domestic', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='소속 대학')
+    user_email = models.EmailField(blank=True, verbose_name='이메일')
