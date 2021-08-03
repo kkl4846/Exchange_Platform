ce3363b7e753df403b9f141c73a7f124870a7017
Minji Kwak <beinte0419@snu.ac.kr>
2021-08-03 16:41:57 +0900

    Fix User 모델 수정

--- login/models.py
+++ login/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 
-class User(AbstractBaseUser):
-    nickname = models.CharField(max_length=20, verbose_name='닉네임')
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, verbose_name='아이디', unique=True,
+                                help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.')
+    nickname = models.CharField(max_length=20, verbose_name='닉네임', unique=True)
     university = models.ForeignKey(
         'domestic.Domestic', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='소속 대학')
-    user_email = models.EmailField(blank=True, verbose_name='이메일')
+    user_email = models.EmailField(blank=True, verbose_name='이메일', unique=True)
+    USERNAME_FIELD = 'username'
