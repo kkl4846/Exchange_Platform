7cee860cf0a77915b2d0388c165a62e63be4627b
Jimin Kim <0305jimin@gmail.com>
2021-08-03 15:04:43 +0900

    TEST-1 [Test] question model 임의 작성

--- question/models.py
+++ question/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    author = models.ForeignKey(to='login.User')
+    home_university = models.ForeignKey(
+        to='domestic.Domestic University', null=True, blank=True)
+    away_university = models.ForeignKey(
+        to='foreign.Foreign University', null=True, blank=True)
+    country = models.ForeignKey(to='country.Country', null=True, blank=True)
+    question_title = models.CharField(max_length=50)
+    question_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
