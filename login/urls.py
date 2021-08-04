433b7fe53b5fb5e6b915d01b76097216d6b1b8cf
Minji Kwak <beinte0419@snu.ac.kr>
2021-08-04 10:29:48 +0900

    TEST-11 FIX 로그인 버그 수정

--- login/urls.py
+++ login/urls.py
@@ -7,7 +7,7 @@ app_name = 'login'
 urlpatterns = [
     path('', views.user_main, name='user_main'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    # path('login/', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='login/main.html'), name='logout'),
-    path('profile/', views.profile, name='profile'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 # from django.contrib.auth import login as auth_login
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from . import models, forms
 from django.contrib import auth
 
@@ -26,6 +24,20 @@ def signup(request):
         return render(request, 'login/signup.html', {'form': form})
 
 
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('login:user_main')
+        else:
+            return render(request, 'login/login.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request, 'login/login.html')
+
+
 @login_required
 def profile(request):
-    return(request, 'login/profile.html')
+    return render(request, 'login/user_main.html')
