4b328277c1c67430ea7fda22a4e5040de90f941a
jeongkite <jeong6kite@gmail.com>
2021-08-05 22:46:32 +0900

    TEST-6 Fix: country_list view 수정

--- country/views.py
+++ country/views.py
@@ -10,16 +10,18 @@ def country_list(request):
     countries = Country.objects.all().order_by('country_name')
     countries_dict = {}
     last_cho = 'ㄱ'
+    countries_dict[last_cho] = []
 
     for c in countries:
         this_country = c.country_name
         country_cho = j2hcj(h2j(this_country[0]))[0]
-        if last_cho != country_cho:     # 직전 초성과 다른 초성
-            countries_dict[country_cho] = [this_country]
+        if last_cho != country_cho:     # 직전 초성과 다른 초성   
+            countries_dict[country_cho] = []
+            countries_dict[country_cho].append(this_country)
             last_cho = country_cho
         else:                           # 같은 초성
             countries_dict[country_cho].append(this_country)
     
-    print(countries_dict)
+    # print(countries_dict)
 
     return render(request, 'country/country_list.html', {'countries_dict': countries_dict})
