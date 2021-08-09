1eb8241f2f0ebdc739b1964b5d12562afc03f21f
Minji Kwak <beinte0419@snu.ac.kr>
2021-08-09 14:01:19 +0900

    TEST-13 Style settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_settings.SECRET_KEY
+SECRET_KEY = 'django-insecure-)2g-6y7uk-vi4rq&-!8v^p8we0jg1_q5wzn&#87+iipoycj42d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
